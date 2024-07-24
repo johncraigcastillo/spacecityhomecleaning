4af9c0888752c14fe4eb708c7d2b7cb45b9c4ca0
johncastillo <johncraigcastillo@gmail.com>
2024-07-24 06:57:59 -0500

    first commit

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Space City Home Cleaning | Residential & Commercial Cleaning Services in Houston, TX`,
+	description: `Professional Residential & Commercial Cleaning Services in Houston, TX. `,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `Space City Home Cleaning`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: true,
+	showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
