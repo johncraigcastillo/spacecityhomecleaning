0352fd068fedddc6b074f07ff3c6060a035b6632
johncastillo <johncraigcastillo@gmail.com>
2024-07-24 07:18:19 -0500

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import lit from '@astrojs/lit';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://spacecityhomecleaning.github.io', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-	sitemap: false, // Generate sitemap (set to "false" to disable)
-	integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  site: 'https://spacecityhomecleaning.github.io',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: false,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [sitemap(), mdx(), lit(), icon()] // Add renderers to the config
+  ,
+  output: "server",
+  adapter: netlify()
 });
-
